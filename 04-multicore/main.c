e0becb98f8f77100fb8c512480d31f587fe06908
Daniel Bertalan <dani@danielbertalan.dev>
2024-07-23 15:19:56 +0200

    remove test code

--- 04-multicore/main.c
+++ 04-multicore/main.c
@@ -4,8 +4,6 @@ void(* volatile cpu1_job)() = 0;
 
 void cpu1_main()
 {
-    drawChar('l', 3, 0, 0x0F);
-    drawChar('l', 4, 0, 0x0F);
 }
 
 void run_on_cpu1(void(*func)())
@@ -17,8 +15,6 @@ void run_on_cpu1(void(*func)())
 void kernel_main()
 {
     fb_init();
-    drawChar('H', 0, 0, 0x0F);
-    drawChar('e', 1, 0, 0x0F);
 
     run_on_cpu1(cpu1_main);
 
